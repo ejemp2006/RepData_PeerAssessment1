d2e4c6a31fbbcb10306019c5bb295993eeebf905
ejemp2006 <34761788+ejemp2006@users.noreply.github.com>
2018-02-24 14:24:32 +0900

    Delete 6_total_steps_hist.R

--- 6_total_steps_hist.R
+++
@@ -1,3 +0,0 @@
-steps_each_day_complete <- aggregate(steps ~ date, data = complete_data, sum)
-colnames(steps_each_day_complete) <- c("date", "steps")
-hist(as.numeric(steps_each_day_complete$steps), breaks = 20, col = "red", xlab = "Number of Steps", main= "Histogram of the total number of steps taken each day")
